e2be29f2f1fca47004a48ad86f53fb124db1d01e
ginlink <ginlink1998@gmail.com>
2022-05-04 11:46:55 +0800

    feat: add multi env

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { baseUrl } from './service/config';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <p>baseUrl: {baseUrl}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
